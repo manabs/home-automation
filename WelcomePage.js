af0213eaa0d0c6d44e132a16c042f54d734b815c
manabs <manabalalams@gmail.com>
2023-09-23 17:03:03 +0200

    Update WelcomePage.js

--- WelcomePage.js
+++ WelcomePage.js
@@ -9,10 +9,9 @@ export default function WelcomePage({ navigation }) {
   return (
     <Background>
       <Logo />
-      <Header>Let’s start</Header>
+      <Header></Header>
       <Paragraph>
-        Your amazing app starts here. Open you favorite code editor and start
-        editing this project.
+        
       </Paragraph>
       <Button
         mode="outlined"
